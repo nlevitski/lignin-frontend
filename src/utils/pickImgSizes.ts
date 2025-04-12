fb7b2417593444c4a4780a18f242b8d7d9e3c26b
TheLevius <reflevius@gmail.com>
2025-04-12 17:02:52 +0300

    fix: delete unused util

--- src/utils/pickImgSizes.ts
+++
@@ -1,7 +0,0 @@
-import { Formats, ImageProperties } from '@/dal/articles';
-
-export const pickImgSize = (obj: Formats): ImageProperties => {
-	const sizes = ['large', 'medium', 'small', 'thumbnail'] as const;
-	const field = sizes.find((size) => Object.hasOwn(obj, size)) || 'small';
-	return obj[field];
-};
