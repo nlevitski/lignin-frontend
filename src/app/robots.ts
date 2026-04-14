803dff10a0f9284349dde1384d7f3fad670b91c7
nlevitski <reflevius@gmail.com>
2026-04-14 11:47:43 +0300

    feat: open cdn-cgi

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,13 +1,13 @@
-import { MetadataRoute } from 'next';
-import { getSiteUrl } from '@/utils/siteUrl';
+import { MetadataRoute } from "next";
+import { getSiteUrl } from "@/utils/siteUrl";
 
 const baseUrl = getSiteUrl();
 export default function robots(): MetadataRoute.Robots {
 	return {
 		rules: {
-			userAgent: '*',
-			allow: '/',
-			disallow: ['/admin', '/cdn-cgi/*'],
+			userAgent: "*",
+			allow: "/",
+			disallow: ["/admin"],
 		},
 		sitemap: `${baseUrl}/sitemap.xml`,
 	};
